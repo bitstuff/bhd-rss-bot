435489f1c7b3f00fdd741d6e9ddbb61e3e246af2
bitstuff <bitstuff@users.noreply.github.com>
2020-10-19 12:25:46 -0700

    removing some of the chatter

--- src/main.rs
+++ src/main.rs
@@ -9,7 +9,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     let mut seen: HashMap<String, bool> = HashMap::new();
     let config = config::new();
 
-    let file_re = Regex::new(r"(/[^/]+)$").unwrap();
+    let file_re = Regex::new(r"(/[^/]+)$")?;
     for monitor in &config.monitors {
         let rssxml = reqwest::blocking::get(&monitor.url)?
             .text()?;
@@ -24,13 +24,13 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
                     let content = reqwest::blocking::get(&item.link)?
                         .bytes()?;
                     let mut filename = config.dropdir.clone();
-                    println!("guid: {}", item.guid);
+                    //println!("guid: {}", item.guid);
                     let capture: Vec<regex::Captures> = file_re
                         .captures_iter(&item.guid)
                         .collect();
                     filename.push_str(&capture[0][0]);
                     filename.push_str(".torrent");
-                    println!("fetching {} to {}", item.guid, filename);
+                    //println!("fetching {} to {}", item.guid, filename);
                     let mut file = File::create(filename)?;
                     file.write_all(&content)?;
                     break;
@@ -39,6 +39,8 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
             seen.insert(item.guid, true);
         }
     }
+
+    //println!("{:#?}", seen);
     
     Ok(())
 }
